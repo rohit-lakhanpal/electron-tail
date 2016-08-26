7258a7c222a00e63ca63b1a2c4a365f68d80f277
Rohit Lakhanpal <mail@rohitl.com>
2016-08-26 17:03:36 +1000

    Added files to move to electron-boilerplate version 4.1.0

--- app/env.js
+++
@@ -1,9 +0,0 @@
-// Simple wrapper exposing environment variables to rest of the code.
-
-import jetpack from 'fs-jetpack';
-
-// The variables have been written to `env.json` by the build process.
-var env = jetpack.cwd(__dirname).read('env.json', 'json');
-env.appUserModelId = '711a32ac-1679-478f-981b-360afca5ce03';
-env.autoUpdateFeedUrl = 'http://ec2-52-64-55-12.ap-southeast-2.compute.amazonaws.com:1337/update'; // TODO: Remove from here & add to env_XX.
-export default env;
